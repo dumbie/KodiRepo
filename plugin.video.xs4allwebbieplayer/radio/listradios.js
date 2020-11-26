d5aca42f2a082e4edf26027da96369bc3ed590d7
Arnold Vink <info@arnoldvink.com>
2020-11-26 11:38:42 +0100

    - Moved Kodi repository to GitHub.

---
+++ plugin.video.xs4allwebbieplayer/radio/listradios.js
@@ -0,0 +1,104 @@
+{
+  "radios": [
+    {
+      "id": "nporadio1",
+      "name": "NPO Radio 1",
+      "stream": "http://icecast.omroep.nl/radio1-bb-mp3"
+    },
+    {
+      "id": "nporadio2",
+      "name": "NPO Radio 2",
+      "stream": "http://icecast.omroep.nl/radio2-bb-mp3"
+    },
+    {
+      "id": "npo3fm",
+      "name": "NPO 3 FM",
+      "stream": "http://icecast.omroep.nl/3fm-bb-mp3"
+    },
+    {
+      "id": "nporadio4",
+      "name": "NPO Radio 4",
+      "stream": "http://icecast.omroep.nl/radio4-bb-mp3"
+    },
+    {
+      "id": "nporadio5",
+      "name": "NPO Radio 5",
+      "stream": "http://icecast.omroep.nl/radio5-bb-mp3"
+    },
+    {
+      "id": "npofunx",
+      "name": "NPO FunX",
+      "stream": "http://icecast.omroep.nl/funx-bb-mp3"
+    },
+    {
+      "id": "nposouljazz",
+      "name": "NPO Soul & Jazz",
+      "stream": "http://icecast.omroep.nl/radio6-bb-mp3"
+    },
+    {
+      "id": "radioveronica",
+      "name": "Radio Veronica",
+      "stream": "http://playerservices.streamtheworld.com/api/livestream-redirect/VERONICA.mp3"
+    },
+    {
+      "id": "kink",
+      "name": "KINK",
+      "stream": "http://playerservices.streamtheworld.com/api/livestream-redirect/KINK.mp3"
+    },
+    {
+      "id": "skyradio",
+      "name": "Sky Radio",
+      "stream": "http://playerservices.streamtheworld.com/api/livestream-redirect/SKYRADIO.mp3"
+    },
+    {
+      "id": "radio538",
+      "name": "Radio 538",
+      "stream": "http://playerservices.streamtheworld.com/api/livestream-redirect/RADIO538.mp3"
+    },
+    {
+      "id": "radio10",
+      "name": "Radio 10",
+      "stream": "http://playerservices.streamtheworld.com/api/livestream-redirect/RADIO10.mp3"
+    },
+    {
+      "id": "qmusic",
+      "name": "Qmusic",
+      "stream": "http://icecast-qmusic.cdp.triple-it.nl/Qmusic_nl_live_96.mp3"
+    },
+    {
+      "id": "slamfm",
+      "name": "Slam FM",
+      "stream": "http://playerservices.streamtheworld.com/api/livestream-redirect/SLAM_MP3.mp3"
+    },
+    {
+      "id": "arrowclassicrock",
+      "name": "Arrow Classic Rock",
+      "stream": "http://stream.gal.io/arrowcr"
+    },
+    {
+      "id": "sublimefm",
+      "name": "Sublime FM",
+      "stream": "http://stream01.sublimefm.nl/SublimeFM_mp3"
+    },
+    {
+      "id": "classicfm",
+      "name": "Classic FM",
+      "stream": "http://playerservices.streamtheworld.com/api/livestream-redirect/CLASSICFM.mp3"
+    },
+    {
+      "id": "100pnl",
+      "name": "100% Puur NL",
+      "stream": "http://playerservices.streamtheworld.com/api/livestream-redirect/100PNL_MP3.mp3"
+    },
+    {
+      "id": "piratenfamilie",
+      "name": "Piraten Familie",
+      "stream": "http://server-27.stream-server.nl:8154/stream.mp3"
+    },
+    {
+      "id": "grolloo",
+      "name": "Grolloo Radio",
+      "stream": "https://uk1.streamingpulse.com/ssl/grollooradio"
+    }
+  ]
+}
