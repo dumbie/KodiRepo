af565ffe94d4c34f08034ab86549163d9bb8ef47
Arnold Vink <info@arnoldvink.com>
2021-03-19 18:16:02 +0100

    - Added plugin.video.vogelspot stream files.

---
+++ plugin.video.vogelspot/streams.js
@@ -0,0 +1,30 @@
+[
+  {
+    "id": "bosuil",
+    "name": "Bosuil Binnen (Meinweg, Limburg)",
+    "source": "Beleefdelente",
+    "stream": "https://rrr.sz.xlcdn.com/?account=bdl&file=Bosuil1&type=live&service=wowza&protocol=https&output=playlist.m3u8",
+    "token": "https://www.vogelbescherming.nl/cfc/bdl/CFJetStream/JetStream.cfc?method=GenerateToken&streamname=Bosuil1"
+  },
+  {
+    "id": "bosuil",
+    "name": "Bosuil Buiten (Meinweg, Limburg)",
+    "source": "Beleefdelente",
+    "stream": "https://rrr.sz.xlcdn.com/?account=bdl&file=Bosuil2&type=live&service=wowza&protocol=https&output=playlist.m3u8",
+    "token": "https://www.vogelbescherming.nl/cfc/bdl/CFJetStream/JetStream.cfc?method=GenerateToken&streamname=Bosuil2"
+  },
+  {
+    "id": "steenuil",
+    "name": "Steenuil Binnen (Sprundel, Noord-Brabant)",
+    "source": "Nestkastlive",
+    "stream": "https://5d881567b7568.streamlock.net/live/Steenuil2Binnen.stream/playlist.m3u8",
+    "token": ""
+  },
+  {
+    "id": "steenuil",
+    "name": "Steenuil Buiten (Sprundel, Noord-Brabant)",
+    "source": "Nestkastlive",
+    "stream": "https://5d881567b7568.streamlock.net/live/Steenuil2Buiten.stream/playlist.m3u8",
+    "token": ""
+  }
+]
