c6750045cf04c6b30c85f39d6db007d76a051819
Arnold Vink <vink_arnold@hotmail.com>
2024-03-04 16:42:30 +0100

    - Radio streams aangepast.

--- plugin.video.xs4allwebbieplayer/radio/listradios.js
+++ plugin.video.xs4allwebbieplayer/radio/listradios.js
@@ -1,114 +1,294 @@
 {
   "radios": [
     {
-      "id": "nporadio1",
+      "id": "801",
       "name": "NPO Radio 1",
       "stream": "https://icecast.omroep.nl/radio1-bb-mp3"
     },
     {
-      "id": "nporadio2",
+      "id": "802",
       "name": "NPO Radio 2",
       "stream": "https://icecast.omroep.nl/radio2-bb-mp3"
     },
     {
-      "id": "npo3fm",
-      "name": "NPO 3 FM",
+      "id": "803",
+      "name": "NPO 3FM",
       "stream": "https://icecast.omroep.nl/3fm-bb-mp3"
     },
     {
-      "id": "nporadio4",
-      "name": "NPO 4 Klassiek",
+      "id": "804",
+      "name": "NPO Klassiek",
       "stream": "https://icecast.omroep.nl/radio4-bb-mp3"
     },
     {
-      "id": "nporadio5",
+      "id": "805",
       "name": "NPO Radio 5",
       "stream": "https://icecast.omroep.nl/radio5-bb-mp3"
     },
     {
-      "id": "npofunx",
-      "name": "NPO FunX",
-      "stream": "https://icecast.omroep.nl/funx-bb-mp3"
-    },
-    {
-      "id": "nposterren",
-      "name": "NPO Sterren",
-      "stream": "https://icecast.omroep.nl/radio2-sterrennl-mp3"
-    },
-    {
-      "id": "nposouljazz",
+      "id": "806",
       "name": "NPO Soul & Jazz",
       "stream": "https://icecast.omroep.nl/radio6-bb-mp3"
     },
     {
-      "id": "radioveronica",
-      "name": "Radio Veronica",
-      "stream": "https://playerservices.streamtheworld.com/api/livestream-redirect/VERONICA.mp3"
+      "id": "807",
+      "name": "NPO Sterren",
+      "stream": "https://icecast.omroep.nl/radio2-sterrennl-mp3"
     },
     {
-      "id": "kink",
-      "name": "KINK",
-      "stream": "https://playerservices.streamtheworld.com/api/livestream-redirect/KINK.mp3"
-    },
-    {
-      "id": "skyradio",
+      "id": "808",
       "name": "Sky Radio",
       "stream": "https://playerservices.streamtheworld.com/api/livestream-redirect/SKYRADIO.mp3"
     },
     {
-      "id": "radio538",
+      "id": "809",
       "name": "Radio 538",
       "stream": "https://playerservices.streamtheworld.com/api/livestream-redirect/RADIO538.mp3"
     },
     {
-      "id": "radio10",
-      "name": "Radio 10",
-      "stream": "https://playerservices.streamtheworld.com/api/livestream-redirect/RADIO10.mp3"
-    },
-    {
-      "id": "qmusic",
-      "name": "Qmusic",
-      "stream": "https://icecast-qmusicnl-cdp.triple-it.nl/Qmusic_nl_live.mp3"
-    },
-    {
-      "id": "joe",
+      "id": "810",
       "name": "Joe",
       "stream": "https://icecast-qmusicnl-cdp.triple-it.nl/Joe_nl.mp3"
     },
     {
-      "id": "slamfm",
-      "name": "SLAM!",
-      "stream": "https://playerservices.streamtheworld.com/api/livestream-redirect/SLAM_MP3.mp3"
+      "id": "811",
+      "name": "Qmusic",
+      "stream": "https://icecast-qmusicnl-cdp.triple-it.nl/Qmusic_nl_live.mp3"
     },
     {
-      "id": "arrowclassicrock",
+      "id": "812",
+      "name": "Classic NL",
+      "stream": "https://stream.classic.nl/classicnl.mp3"
+    },
+    {
+      "id": "813",
+      "name": "Radio 10",
+      "stream": "https://playerservices.streamtheworld.com/api/livestream-redirect/RADIO10.mp3"
+    },
+    {
+      "id": "814",
       "name": "Arrow Classic Rock",
       "stream": "https://stream.player.arrow.nl/arrowcr"
     },
     {
-      "id": "sublimefm",
-      "name": "Sublime FM",
+      "id": "815",
+      "name": "BNR Nieuwsradio",
+      "stream": "https://playerservices.streamtheworld.com/api/livestream-redirect/BNR_NIEUWSRADIO.mp3"
+    },
+    {
+      "id": "816",
+      "name": "SLAM!",
+      "stream": "https://playerservices.streamtheworld.com/api/livestream-redirect/SLAM_MP3.mp3"
+    },
+    {
+      "id": "817",
+      "name": "KINK",
+      "stream": "https://playerservices.streamtheworld.com/api/livestream-redirect/KINK.mp3"
+    },
+    {
+      "id": "818",
+      "name": "Sublime",
       "stream": "https://playerservices.streamtheworld.com/api/livestream-redirect/SUBLIME.mp3"
     },
     {
-      "id": "classicfm",
-      "name": "Classic FM",
-      "stream": "https://playerservices.streamtheworld.com/api/livestream-redirect/CLASSICFM.mp3"
+      "id": "819",
+      "name": "Radio Veronica",
+      "stream": "https://playerservices.streamtheworld.com/api/livestream-redirect/VERONICA.mp3"
     },
     {
-      "id": "100pnl",
-      "name": "100% Puur NL",
+      "id": "820",
+      "name": "Sunlite",
+      "stream": "https://playerservices.streamtheworld.com/api/livestream-redirect/SUNLITE_MP3.mp3"
+    },
+    {
+      "id": "821",
+      "name": "NPO FunX",
+      "stream": "https://icecast.omroep.nl/funx-bb-mp3"
+    },
+    {
+      "id": "822",
+      "name": "Radio Decibel",
+      "stream": "https://playerservices.streamtheworld.com/api/livestream-redirect/RADIODECIBEL.mp3"
+    },
+    {
+      "id": "823",
+      "name": "100% NL",
       "stream": "https://playerservices.streamtheworld.com/api/livestream-redirect/100PNL_MP3.mp3"
     },
     {
-      "id": "piratenfamilie",
+      "id": "824",
+      "name": "Concertzender",
+      "stream": "http://streams.greenhost.nl:8080/live"
+    },
+    {
+      "id": "825",
+      "name": "Radio NL",
+      "stream": "https://stream.radionl.fm/radionl"
+    },
+    {
+      "id": "826",
+      "name": "Candlelight Radio",
+      "stream": "https://playerservices.streamtheworld.com/api/livestream-redirect/CANDLELIGHT.mp3"
+    },
+    {
+      "id": "827",
+      "name": "Radio Continu",
+      "stream": "https://stream.radiocontinu.nl/radiocontinu"
+    },
+    {
+      "id": "829",
+      "name": "Groot Nieuws Radio",
+      "stream": "https://playerservices.streamtheworld.com/api/livestream-redirect/GNR.mp3"
+    },
+    {
+      "id": "830",
+      "name": "Radio 509",
+      "stream": "https://playerservices.streamtheworld.com/api/livestream-redirect/RADIO509.mp3"
+    },
+    {
+      "id": "831",
+      "name": "Efteling Kids Radio",
+      "stream": "https://i2.cdn.jetstre.am:8000/sz=efteling=hdstream"
+    },
+    {
+      "id": "832",
+      "name": "Grand Prix Radio",
+      "stream": "https://playerservices.streamtheworld.com/api/livestream-redirect/GRAND_PRIX_RADIO.mp3"
+    },
+    {
+      "id": "841",
+      "name": "VRT Radio 1",
+      "stream": "https://icecast.vrtcdn.be/radio1-high.mp3"
+    },
+    {
+      "id": "842",
+      "name": "VRT Klara",
+      "stream": "https://icecast.vrtcdn.be/klara-high.mp3"
+    },
+    {
+      "id": "843",
+      "name": "VRT MNM",
+      "stream": "https://icecast.vrtcdn.be/mnm-high.mp3"
+    },
+    {
+      "id": "844",
+      "name": "Studio Brussel",
+      "stream": "https://icecast.vrtcdn.be/stubru-high.mp3"
+    },
+    {
+      "id": "847",
+      "name": "VRT Radio 2",
+      "stream": "https://icecast.vrtcdn.be/ra2vlb-high.mp3"
+    },
+    {
+      "id": "850",
+      "name": "Deutschlandfunk",
+      "stream": "https://st01.sslstream.dlf.de/dlf/01/128/mp3/stream.mp3"
+    },
+    {
+      "id": "861",
+      "name": "BBC1",
+      "stream": "https://as-hls-ww.live.cf.md.bbci.co.uk/pool_904/live/ww/bbc_radio_one/bbc_radio_one.isml/bbc_radio_one.m3u8"
+    },
+    {
+      "id": "862",
+      "name": "BBC2",
+      "stream": "https://as-hls-ww.live.cf.md.bbci.co.uk/pool_904/live/ww/bbc_radio_two/bbc_radio_two.isml/bbc_radio_two.m3u8"
+    },
+    {
+      "id": "863",
+      "name": "BBC3",
+      "stream": "https://as-hls-ww.live.cf.md.bbci.co.uk/pool_904/live/ww/bbc_radio_three/bbc_radio_three.isml/bbc_radio_three.m3u8"
+    },
+    {
+      "id": "864",
+      "name": "BBC4",
+      "stream": "https://as-hls-ww.live.cf.md.bbci.co.uk/pool_904/live/ww/bbc_radio_fourfm/bbc_radio_fourfm.isml/bbc_radio_fourfm.m3u8"
+    },
+    {
+      "id": "865",
+      "name": "BBC World Service",
+      "stream": "https://as-hls-ww.live.cf.md.bbci.co.uk/pool_904/live/ww/bbc_world_service/bbc_world_service.isml/bbc_world_service.m3u8"
+    },
+    {
+      "id": "866",
+      "name": "Sunrise FM",
+      "stream": "https://radio.sfmstreaming.nl:8006/stream"
+    },
+    {
+      "id": "880",
+      "name": "Omrop Fryslân",
+      "stream": "https://d3pvma9xb2775h.cloudfront.net/icecast/omropfryslan/radio.mp3"
+    },
+    {
+      "id": "881",
+      "name": "RTV Noord",
+      "stream": "https://media.rtvnoord.nl/icecast/rtvnoord/radio"
+    },
+    {
+      "id": "882",
+      "name": "RTV Drenthe",
+      "stream": "https://cdn.rtvdrenthe.nl/icecast/rtvdrenthe/rtvradio"
+    },
+    {
+      "id": "883",
+      "name": "RTV Oost",
+      "stream": "https://streams.rtvoost.nl/audio/oost/mp3"
+    },
+    {
+      "id": "884",
+      "name": "Radio Gelderland",
+      "stream": "https://d2od87akyl46nm.cloudfront.net/icecast/omroepgelderland/radiogelderland"
+    },
+    {
+      "id": "885",
+      "name": "Omroep Flevoland",
+      "stream": "https://stream.omroepflevoland.nl/icecast/omroepflevoland/stream2"
+    },
+    {
+      "id": "886",
+      "name": "Omroep Brabant",
+      "stream": "http://av.omroepbrabant.nl/icecast/omroepbrabant/mp3"
+    },
+    {
+      "id": "887",
+      "name": "RTV Utrecht",
+      "stream": "https://d18rwjdhpr8dcw.cloudfront.net/icecast/rtvutrecht/radiomutrecht-bb-mp3"
+    },
+    {
+      "id": "889",
+      "name": "RTV West",
+      "stream": "https://liveradio.omroepwest.nl"
+    },
+    {
+      "id": "890",
+      "name": "Radio Rijnmond",
+      "stream": "https://dcur8bjarl5c2.cloudfront.net/icecast/rijnmond/radio-mp3"
+    },
+    {
+      "id": "891",
+      "name": "L1 Radio",
+      "stream": "https://d34pj260kw1xmk.cloudfront.net/icecast/l1/radio-bb-mp3"
+    },
+    {
+      "id": "892",
+      "name": "Omroep Zeeland",
+      "stream": "https://d3isaxd2t6q8zm.cloudfront.net/icecast/omroepzeeland/omroepzeeland_radio"
+    },
+    {
+      "id": "900",
       "name": "Piraten Familie",
       "stream": "http://server-27.stream-server.nl:8154/stream.mp3"
     },
     {
-      "id": "grolloo",
+      "id": "901",
       "name": "Grolloo Radio",
       "stream": "https://uk1.streamingpulse.com/ssl/grollooradio"
+    },
+    {
+      "id": "902",
+      "name": "Tukker FM",
+      "stream": "https://stream.tukkerfm.nl/tukkerfm"
     }
   ]
 }
